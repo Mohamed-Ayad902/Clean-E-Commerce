9ff56814f19645b6298b7e10a4983cc6bac183cc
Mohamed-Ayad902 <mohamed.ayad7474@gmail.com>
2024-12-29 17:15:16 +0200

    added required dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,11 +2,16 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    alias(libs.plugins.dagger.hilt.android)
+    alias(libs.plugins.kapt)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
 }
 
 android {
     namespace = "com.mayad7474.cleane_commerce"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.mayad7474.cleane_commerce"
@@ -56,4 +61,47 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Hilt - dependency injection
+    implementation(libs.hilt.android)
+    // hilt kapt - annotation processor — hilt ksp is in alpha
+    kapt(libs.hilt.compiler)
+
+    // Retrofit - make type safe requests
+    implementation(libs.retrofit)
+    // okhttp - fire http calls
+    implementation(libs.okhttp)
+    // logging interceptor - log calls for debugging
+    implementation(libs.logging.interceptor)
+    // converter - make retrofit use serialization json
+    implementation(libs.converter.kotlinx.serialization)
+    // JSON - serialize Kotlin objects <-> JSON
+    implementation(libs.kotlinx.serialization.json)
+
+    // Coil - asynchronous image loading
+    implementation(libs.coil.compose)
+
+    // datastore - make type safe local storage
+    implementation(libs.androidx.datastore)
+
+    // room - local database
+    implementation(libs.androidx.room.runtime)
+    // room ksp - annotation processor
+    ksp(libs.androidx.room.compiler)
+    // room - coroutine support
+    implementation(libs.androidx.room.ktx)
+
+    // kotlin coroutines tests
+    testImplementation(libs.kotlinx.coroutines.test)
+
+    // collect as state with lifecycle
+    implementation(libs.androidx.lifecycle.runtime.compose)
+
+    // compose nav host & type safe navigation
+    implementation(libs.androidx.navigation.compose)
+    // hilt view model injection
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    // lottie with compose
+    implementation(libs.lottie)
 }
